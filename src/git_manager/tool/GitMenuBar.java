c1ae313ff2644bdd55deae59d3bc02a1a8ee0e2c
joelmoniz <jramoniz@gmail.com>
2014-03-21 18:29:09 +0530

    Updated Git option of Menu Bar

--- src/git_manager/tool/GitMenuBar.java
+++ src/git_manager/tool/GitMenuBar.java
@@ -33,25 +33,48 @@ public class GitMenuBar extends JMenuBar {
 	}
 
 	// TODO: Fill this up, and give options proper names
-	// This is only a list as and when it comes into my mind for now, not
-	// necessarily complete nor necessarily sufficiently meaningful
+	// This is only a list as and when various useful options come into my mind
+	// for now, not
+	// necessarily complete (nor necessarily sufficiently meaningful)
+
+	// TODO: Add option to work with remote
+
+	// TODO: Provide a fetch option?
+
+	// TODO: Provide tag-related options
 	private void populateGitMenu() {
+
+		JMenu submenuCommit = new JMenu("Edit Commits");
+		JMenu submenuStash = new JMenu("Stash");
+		JMenu submenuBranch = new JMenu("Branch");
+
 		JMenuItem init = new JMenuItem("Initialize repo");
 		JMenuItem add = new JMenuItem("Add");
+		JMenuItem unstage = new JMenuItem("Unstage an added file");
 		JMenuItem commit = new JMenuItem("Commit");
+		JMenuItem amend = new JMenuItem("Modify last commit");
+		JMenuItem checkoutFile = new JMenuItem("Unmodify a file");
 		JMenuItem snapshot = new JMenuItem("Snapshot");
 		JMenuItem diff = new JMenuItem("Difference");
+		JMenuItem commitHistory = new JMenuItem("Commit History");
 		JMenuItem push = new JMenuItem("Push");
-		JMenuItem revert = new JMenuItem("Revert");
+		JMenuItem revert = new JMenuItem("Undo a commit");
 		JMenuItem remove = new JMenuItem("Remove");
 		JMenuItem status = new JMenuItem("Status");
 		JMenuItem ignore = new JMenuItem("Ignore Files");
-		JMenuItem rebase = new JMenuItem("Rebase");
+
+		JMenuItem rebaseSplit = new JMenuItem("Split a Commit");
+		JMenuItem rebaseSquash = new JMenuItem("Squash Commits");
+		JMenuItem rebaseChangeMessage = new JMenuItem("Change Commit Messages");
+		JMenuItem rebaseReorder = new JMenuItem("Reorder Commits");
+
 		JMenuItem stash = new JMenuItem("Save into Stash");
 		JMenuItem stash_list = new JMenuItem("View Stash");
 		JMenuItem stash_apply = new JMenuItem("Apply Stashed Changes");
 		JMenuItem branch = new JMenuItem("Create Branch");
-		JMenuItem checkout = new JMenuItem("Change Branch");
+		JMenuItem checkoutBranch = new JMenuItem("Change Branch");
+		JMenuItem mergeBranch = new JMenuItem("Merge Branch");
+		JMenuItem deleteBranch = new JMenuItem("Delete Branch");
 		JMenuItem clone = new JMenuItem("Clone a repo");
 		JMenuItem readMe = new JMenuItem("Add a Read Me file");
 
@@ -59,29 +82,59 @@ public class GitMenuBar extends JMenuBar {
 		git.add(snapshot);
 		git.add(add);
 		git.add(commit);
-				
-		git.add(push);
-		git.add(revert);
-		git.add(remove);
-		
-		git.add(rebase);
-		git.add(stash);
-		git.add(stash_list);
-		git.add(stash_apply);
-		git.add(branch);
-		git.add(checkout);
+
+		git.addSeparator();
+
 		git.add(clone);
-		
+		git.add(push);
+
+		git.addSeparator();
+
+		git.add(checkoutFile);
+		git.add(unstage);
+		git.add(remove);
+
+		git.addSeparator();
+
+		submenuCommit.add(amend);
+		submenuCommit.add(amend);
+		submenuCommit.add(revert);
+		submenuCommit.add(rebaseChangeMessage);
+		submenuCommit.add(rebaseSplit);
+		submenuCommit.add(rebaseSquash);
+		submenuCommit.add(rebaseReorder);
+		git.add(submenuCommit);
+
+		git.addSeparator();
+
+		submenuStash.add(stash);
+		submenuStash.add(stash_list);
+		submenuStash.add(stash_apply);
+		git.add(submenuStash);
+
+		git.addSeparator();
+
+		submenuBranch.add(branch);
+		submenuBranch.add(checkoutBranch);
+		submenuBranch.add(mergeBranch);
+		submenuBranch.add(deleteBranch);
+		git.add(submenuBranch);
+
+		git.addSeparator();
+
 		git.add(diff);
 		git.add(status);
-		
+		git.add(commitHistory);
+
+		git.addSeparator();
+
 		git.add(ignore);
 		git.add(readMe);
 
 	}
 
 	private void populateSettingsMenu() {
-		JMenuItem config = new JMenuItem("Git config");
+		JMenuItem config = new JMenuItem("Specify Git Config Settings");
 		JMenuItem gitToolBarSet = new JMenuItem("Git Option Toolbar");
 		JMenuItem expLevMenuSet = new JMenuItem("Expertise Level Menu");
 		JMenuItem repoSet = new JMenuItem("Local Repository");
